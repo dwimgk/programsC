6172287403c1fc7b0bed35e7a67a900af119f72d
dwimgk <damyan.chakarov@gmail.com>
2024-10-03 14:53:11 +0300

    Create mario-more.c
    double pyramid with input number

---
+++ mario-more.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+{
+    int height = get_int("Pyramid height?\n");
+
+
+    for (int i = 1; i <= height; i++)
+    {
+        for(int length = height -i; length > 0; length = length - 1)
+        {
+            printf(" ");
+        }
+        for(int j=0; j<i;j++)
+        {
+            printf("#");
+        }
+        printf(" ");
+        for(int k=0; k<i;k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
