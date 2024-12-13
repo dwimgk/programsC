5dd74fa55c803819f0a962540a9608889bb71cad
dwimgk <damyan.chakarov@gmail.com>
2024-12-13 22:02:36 +0200

    finish CourseProject

--- CourseProject/functions.c
+++ CourseProject/functions.c
@@ -9,6 +9,8 @@
 
 #include "globals.h"
 
+
+
 void game_over()
 {
     for (int shape_row = 0; shape_row < TETROMINO_ROWS; shape_row++)
@@ -387,4 +389,147 @@ void end_screen()
     printf("\n");
     printf("\n");
     printf("\n");
+    int return_input;
+    printf("TO RETURN TO THE MENU PLEASE TYPE 1 --> ");
+    while (true)
+    {
+        scanf("%d", &return_input);
+        if (return_input == 1)
+        {
+            return;
+        }
+        else
+        {
+            printf("\n Cannot return, input is invalid :(\n");
+        }
+    }
+}
+
+
+// THE GAME LOOP
+void game_init()
+{
+    memcpy(non_dynamic_playfield, playfield, sizeof(non_dynamic_playfield));
+    // seeding with time(null) so that every random num will be different each iteration of the code
+    srand(time(NULL));
+
+    system("cls");
+
+    bool game_running = true;
+    current_shape_isplaced = false;
+
+    pos_row = start_pos_row;
+    pos_col = start_pos_col;
+
+    shape_initialiser();
+
+    while (game_running)
+    {
+        if (game_stop)
+        {
+            end_screen();
+            return;
+        }
+
+        cannot_move_to_left = false;
+        cannot_move_to_right = false;
+
+        position_shapes();
+        print_playfield();
+        if (current_shape_isplaced)
+        {
+            memcpy(non_dynamic_playfield, playfield, sizeof(non_dynamic_playfield));
+            shape_initialiser();
+            pos_row = start_pos_row;
+            pos_col = start_pos_col;
+            current_shape_isplaced = false;
+        }
+        else if (!current_shape_isplaced)
+        {
+            memcpy(playfield, non_dynamic_playfield, sizeof(playfield));
+            pos_row++;
+        }
+        long sleep_time = 150000 - score * 20000;
+        if (sleep_time < 0)
+        {
+            sleep_time = 0;
+        }
+        usleep(sleep_time);
+        // 50 000 microseconds = 0.5s
+
+        proccess_input();
+        clear_input();
+
+        line_checker();
+    }
+    return;
+}
+void scoreboard()
+{
+    int return_input;
+    system("cls");
+    FILE *file = fopen("score.txt", "r");
+    if (file == NULL)
+    {
+        perror("error opening file");
+        return;
+    }
+
+    char ch;
+    while ((ch = fgetc(file)) != EOF)
+    {
+        putchar(ch);
+    }
+
+    fclose(file);
+    printf("TO RETURN TO THE MENU PLEASE TYPE 1 --> ");
+    while (true)
+    {
+        scanf("%d", &return_input);
+        if (return_input == 1)
+        {
+            return;
+        }
+        else
+        {
+            printf("\n Cannot return, input is invalid :(\n");
+        }
+    }
+}
+void play()
+{
+    char player[50];
+
+    printf("Enter player name: ");
+    scanf("%49s", player);
+    // if (player == NULL)
+    // {
+    //     printf("could not allocate memory\n");
+    //     return;
+    // }
+
+    // printf("Enter player name: ");
+
+    // fgets(player, 100, stdin);
+    size_t length = strlen(player);
+    if (length > 0 && player[length - 1] == '\n')
+    {
+        player[length - 1] = '\0';
+    }
+
+    game_init();
+
+    FILE *file = fopen("score.txt", "a");
+    if (file == NULL)
+    {
+        perror("failed to open file");
+        return;
+    }
+    fprintf(file, "Player: %s\n", player);
+    fprintf(file, "Score: %d\n\n", score);
+
+    // Close the file
+    fclose(file);
+
+    return;
 }
--- CourseProject/functions.h
+++ CourseProject/functions.h
@@ -1,8 +1,11 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+
+
 void print_playfield();
 void game_over();
+
 
 void clear_input();
 void bottom_collision(int shape_row, int shape_col);
@@ -14,7 +17,10 @@ void position_shapes();
 void rearranger(int row_to_clear);
 void line_checker();
 
-
 void end_screen();
+
+void game_init();
+void play();
+void scoreboard();
 
 #endif  // FUNCTIONS_H
